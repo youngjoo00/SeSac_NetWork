601dd4fbd84466940efb1492baec74588f4509e9
youngjoo00 <yungjoo0624@gmail.com>
2024-01-16 16:31:53 +0900

    first commit

---
+++ SeSac_Network/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SeSac_Network
+//
+//  Created by youngjoo on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
